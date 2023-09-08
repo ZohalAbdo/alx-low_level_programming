3055d3ec2fa7cefa48313b4b0195f322ad0f9cf7
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2023-09-08 16:32:46 -0700

    all done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 7; i++)
+{
+int j;
+for (j = i + 1; j <= 8; j++)
+{
+int k;
+for (k = j + 1; k <= 9; k++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar('0' + k);
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
