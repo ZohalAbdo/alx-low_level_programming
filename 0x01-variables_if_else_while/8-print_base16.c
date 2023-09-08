e80f1454a631abb807651a651e22d4a465c2886a
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2023-09-08 16:01:26 -0700

    all done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+for (char digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
+
+for (char letter = 'a'; letter <= 'f'; letter++)
+{
+utchar(letter);
+}
+
+putchar('\n');
+
+return (0);
+}
