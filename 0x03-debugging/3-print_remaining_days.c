f58873b3fe6f95c707ce5535f7053826ca6a9b82
Zohal Mohammed <zohalmohammedabdohaj@gmail.com>
2023-09-20 22:47:31 +0400

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,32 +3,32 @@
 /**
  * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
+ * @months: month in number format
+ * @days: day of month
+ * @years: year
  * Return: void
  */
-void print_remaining_days(int month, int day, int year)
+void print_remaining_days(int months, int days, int years)
 {
-if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+if ((years % 100 == 0 && years % 400 == 0) || (years % 4 == 0))
 {
-if (month > 2 && day >= 60)
+if (months > 2 && days >= 60)
 {
-day++;
+days++;
 }
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
-}
-else
-{
-if (month == 2 && day == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+printf("Day of the year: %d\n", days);
+printf("Remaining days: %d\n", 366 - days);
 }
 else
 {
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
+if (months == 2 && days == 60)
+{
+printf("Invalid date: %02d/%02d/%04d\n", months, days - 31, years);
+}
+else
+{
+printf("Day of the year: %d\n", days);
+printf("Remaining days: %d\n", 365 - days);
 }
 }
 }
