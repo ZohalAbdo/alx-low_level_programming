fd28ee1eafb703687f996edc33e75c55d6526d51
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2023-09-21 10:43:46 -0700

    all done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat -concatenates two strings
+ * @dest: first string
+ * @src: secound string
+ *
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+	
+	i = 0;
+	for ( ; dest[i] != '\0'; )
+	{
+		i++;
+	}
+	j = 0;
+	for ( ; src[j] != '\0'; )
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+here is 0x06. C - More pointers, arrays and strings
