82cf88d6fd430fc63bc4f8ff2de0fb30c86ed7fa
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2023-09-25 16:13:24 -0700

    all done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  *@s: first variable
  *@c: second variable
  *
- * Return: pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+ * Return: pointer to the first occurrence, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
