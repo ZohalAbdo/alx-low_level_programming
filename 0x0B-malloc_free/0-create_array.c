2d478a67943bbc318bab8d6c09a98804dc8aa96a
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2023-10-04 14:18:17 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	char_array = malloc((size + 1) * sizeof(char));
+	char_array = malloc((size) * sizeof(char));
 
 	if (char_array != NULL)
 	{
