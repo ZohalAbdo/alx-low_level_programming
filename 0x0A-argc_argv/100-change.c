567e8ef5507cd490abc72e639c0b883b6f3ffdee
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2023-10-04 17:15:37 -0700

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,68 +1,45 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 /**
- * func - Helper function
- * @i: counter
- * Return: int
- */
-int func(int i)
-{
-	int c = 0;
-
-	while (i != 0)
-	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-		c++;
-	}
-	return (c);
-}
-/**
- * main - prints the minimum number of coins
- * @argc: the sum of arguments
- * @argv: array contain values of arguments
+ * main - program that prints the minimum number of coins
+ * to make change for an amount of money.
+ * 
+ * @argc: argument count
+ * @argv: argument value
+ * 
  * Return: 0 or 1
  */
+
 int main(int argc, char *argv[])
 {
-	int i, x;
+	int cents , num_coins , min_coins , i;
+	int *coins;
 
-	x = 0;
-
-	if (argc != 2)
-	{
+	if (argc != 2) {
 		printf("Error\n");
-		return (1);
+		return 1;
 	}
 
-	i = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-	if (i <= 0)
+	if (cents < 0) {
 		printf("0\n");
-	else
-	{
-		x = func(i);
-
-		printf("%d\n", x);
+		return 0;
 	}
 
-	return (0);
+	coins[] = {25, 10, 5, 2, 1};
+	num_coins = sizeof(coins) / sizeof(coins[0]);
+	min_coins = 0;
+
+	for (i = 0; i < num_coins; i++) {
+		while (cents >= coins[i]) {
+			cents -= coins[i];
+			min_coins++;
+		}
+	}
+
+	printf("%d\n", min_coins);
+
+	return 0;
 }
