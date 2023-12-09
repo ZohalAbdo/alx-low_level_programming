0605a999184f51282299a8acaf650b8329fff01c
ZohalAbdo <zohalmohammedabdohaj@gmail.com>
2023-12-08 20:37:16 -0800

    done

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,6 +2,7 @@
 /**
  * add_dnodeint - function that adds a new node at the beginning of list
  *@head: head
+ *@n: value
  *Return:  the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
